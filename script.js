28b2982cc94a8b88604cc9348aa80cd3ba46e76a
samuel lutz <samlutz89@gmail.com>
2022-01-04 20:14:18 -0800

    got the time of day function working

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ var date = moment().format("MMM Do YY");
 //-------append date----------//
 $("#currentDay").append(date);
 
-var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+var timeOfday = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
 updatetime();
 
 // writing a function to update the time
+var timeOfday = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
+updatetime();
+
+function updatetime() {
+    var currentTime = moment().format('H');
+    for(var i = 0; i < timeOfday.length; i++) {
+  
+      if (parseInt(timeOfday[i]) < currentTime) {
+          $("#" + timeOfday[i]).attr("style", "background-color: #c7c7c7");
+  
+      } else if (parseInt(timeOfday[i]) > currentTime) {
+        $("#" + timeOfday[i]).attr("style", "background-color: #49ba6a");
+      
+      } else if (parseInt(timeOfday[i]) == currentTime) {
+        $("#" + timeOfday[i]).attr("style", "background-color: #fa4a41");
+      }
+    }
+  }
