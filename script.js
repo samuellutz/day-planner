956574d036ed8b237793920ce3adfd9b78f96b2d
samuel lutz <samlutz89@gmail.com>
2022-01-05 09:58:09 -0800

    fixing missspelling

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function updatetime() {
 //   getting local storage to save to selected input to proper place
 $(".saveBtn").on("click", function() {
     var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
+    var textContent = $("input").val();
 
     localStorage.setItem(timeOfday, textContent);
     console.log(timeOfday, textContent);
@@ -36,18 +36,3 @@ $(".saveBtn").on("click", function() {
 
 $("#9am").children("input").val(localStorage.getItem("9am"));
 
-$("#10am").children("input").val(localStorage.getItem("10am"));
-
-$("#11am").children("input").val(localStorage.getItem("11am"));
-
-$("#12pm").children("input").val(localStorage.getItem("12pm"));
-
-$("#1pm").children("input").val(localStorage.getItem("1pm"));
-
-$("#2pm").children("input").val(localStorage.getItem("2pm"));
-
-$("#3pm").children("input").val(localStorage.getItem("3pm"));
-
-$("#4pm").children("input").val(localStorage.getItem("4pm"));
-
-$("#5pm").children("input").val(localStorage.getItem("5pm"));
