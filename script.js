45ccb23ad179735af845f3ba23af0a7ac011eb83
samuel lutz <samlutz89@gmail.com>
2022-01-05 09:56:40 -0800

    getting local storage to work properly

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ var date = moment().format("MMM Do YY");
 //-------append date----------//
 $("#currentDay").append(date);
 
-var timeOfday = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
-updatetime();
-
 // writing a function to update the time
 var timeOfday = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
 updatetime();
@@ -26,3 +23,31 @@ function updatetime() {
       }
     }
   }
+
+//   getting local storage to save to selected input to proper place
+$(".saveBtn").on("click", function() {
+    var timeOfday = $(this).parent().attr("id");
+    var textContent = $("input").val().trim();
+
+    localStorage.setItem(timeOfday, textContent);
+    console.log(timeOfday, textContent);
+});
+
+
+$("#9am").children("input").val(localStorage.getItem("9am"));
+
+$("#10am").children("input").val(localStorage.getItem("10am"));
+
+$("#11am").children("input").val(localStorage.getItem("11am"));
+
+$("#12pm").children("input").val(localStorage.getItem("12pm"));
+
+$("#1pm").children("input").val(localStorage.getItem("1pm"));
+
+$("#2pm").children("input").val(localStorage.getItem("2pm"));
+
+$("#3pm").children("input").val(localStorage.getItem("3pm"));
+
+$("#4pm").children("input").val(localStorage.getItem("4pm"));
+
+$("#5pm").children("input").val(localStorage.getItem("5pm"));
